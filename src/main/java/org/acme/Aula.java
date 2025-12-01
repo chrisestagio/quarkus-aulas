4cb256e3376f149627ccb44d23b876ef5f5b3c5f
chrisestagio <chrisestagio06@gmail.com>
2025-12-01 18:42:44 -0300

    Update Aula.java

--- src/main/java/org/acme/Aula.java
+++ src/main/java/org/acme/Aula.java
@@ -1,18 +1,11 @@
 package org.acme;
 
-import jakarta.persistence.Entity;
-import io.quarkus.hibernate.orm.panache.PanacheEntity;
-
-@Entity
-public class Aula extends PanacheEntity {
+public class Aula {
     public int id;
     public String nome;
     public String estilo;
     public String dia;
     public String horario;
-    public Professor professor;
 
-    public Aula(){}
-    
-
+    public Aula() {}
 }
