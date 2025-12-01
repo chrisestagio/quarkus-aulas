dac50fe7443d29bd5dded5addb4cebe6006a175b
chrisestagio <chrisestagio06@gmail.com>
2025-12-01 13:23:10 -0300

    Create Aula class with properties and default constructor

---
+++ src/main/java/org/acme/Aula.java
@@ -0,0 +1,14 @@
+package org.acme;
+
+public class Aula {
+    public int id;
+    public String nome;
+    public String estilo;
+    public String dia;
+    public String horario;
+    public Professor professor;
+
+    public Aula(){}
+    
+
+}
